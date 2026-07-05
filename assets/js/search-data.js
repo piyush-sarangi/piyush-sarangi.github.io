da84f52b737e58eb871e60f6dd29ab92e9a0f6f0
piyush-sarangi <141748829+piyush-sarangi@users.noreply.github.com>
2026-07-05 13:11:56 +0000

    Deploying to gh-pages from @ piyush-sarangi/piyush-sarangi.github.io@fa81a9c33a794aada22f9d8f1a9dc8fc81b19d5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,9 +64,9 @@ ninja.data = [{
           description: "This is a list of reviews of some of the courses at IITKGP",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
